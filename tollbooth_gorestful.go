a014a2a62a203889d626d1aaadd2006ede43bb97
Didip Kerabat <didipk@gmail.com>
2017-09-09 21:19:53 -0700

    Update to conform to v2.x.x API

--- tollbooth_gorestful.go
+++ tollbooth_gorestful.go
@@ -2,15 +2,16 @@ package tollbooth_gorestful
 
 import (
 	"github.com/didip/tollbooth"
-	"github.com/didip/tollbooth/config"
+	"github.com/didip/tollbooth/limiter"
 	"github.com/emicklei/go-restful"
 )
 
-func LimitHandler(handler restful.RouteFunction, limiter *config.Limiter) restful.RouteFunction {
+func LimitHandler(handler restful.RouteFunction, lmt *limiter.Limiter) restful.RouteFunction {
 	return func(request *restful.Request, response *restful.Response) {
-		httpError := tollbooth.LimitByRequest(limiter, request.Request)
+		httpError := tollbooth.LimitByRequest(lmt, request.Request)
 		if httpError != nil {
-			response.WriteErrorString(429, "429: Too Many Requests")
+			response.AddHeader("Content-Type", lmt.GetMessageContentType())
+			response.WriteErrorString(httpError.StatusCode, httpError.Message)
 			return
 		}
 
